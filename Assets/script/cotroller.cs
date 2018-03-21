0ce55146d9c6b9238470c9c2eddf5a83f0e3a732
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-03-21 15:40:23 +0200

    added coroutine rotateAround

--- Assets/script/cotroller.cs
+++ Assets/script/cotroller.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cotroller : MonoBehaviour {
+public class cotroller : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+    }
+
+    IEnumerator RotateAround(Vector3 axis, float angle, float duration)
+    {
+        float elapsed = 0.0f;
+        float rotated = 0.0f;
+        while (elapsed < duration)
+        {
+            float step = angle / duration * Time.deltaTime;
+            transform.RotateAround(transform.position, axis, step);
+            elapsed += Time.deltaTime;
+            rotated += step;
+            yield return null;
+        }
+        transform.RotateAround(transform.position, axis, angle - rotated);
+    }
 }
